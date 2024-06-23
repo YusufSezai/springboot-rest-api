8bafc160098760afb3fb5c8e7afd9b92f820c648
YusufSezai <yusufsezaidogan@gmail.com>
2024-06-23 03:39:31 +0300

    path variable

--- src/main/java/com/example/springboot_rest_api/controller/StudentController.java
+++ src/main/java/com/example/springboot_rest_api/controller/StudentController.java
@@ -3,7 +3,11 @@ package com.example.springboot_rest_api.controller;
 
 import com.example.springboot_rest_api.bean.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class StudentController {
@@ -12,6 +16,22 @@ public class StudentController {
     public Student getStudent() {
         Student student = new Student(1,"George","Lucas");
         return student;
+    }
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
+        List<Student> students = new ArrayList<>();
+
+        students.add(new Student(1,"George","Lucas"));
+        students.add(new Student(2,"John","Doe"));
+        students.add(new Student(3,"Arthur","Lee"));
+
+        return students;
+    }
+
+    @GetMapping("/students/{id}")
+    public Student studentPathVariable(@PathVariable("id") int studentId) {
+        return new Student(studentId, "George","Lucas");
     }
 
 }
