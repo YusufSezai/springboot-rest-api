b91bdc80e0c42d99cbb2f09346371f932701de9f
YusufSezai <yusufsezaidogan@gmail.com>
2024-06-23 21:17:05 +0300

    Post put

--- src/main/java/com/example/springboot_rest_api/controller/StudentController.java
+++ src/main/java/com/example/springboot_rest_api/controller/StudentController.java
@@ -2,10 +2,8 @@ package com.example.springboot_rest_api.controller;
 
 
 import com.example.springboot_rest_api.bean.Student;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -38,6 +36,22 @@ public class StudentController {
     @GetMapping("/students/search")
     public Student studentRequestParam(@RequestParam int id) {
         return new Student(id,"George","Lucas");
+    }
+
+    @PostMapping("students/create")
+    @ResponseStatus(HttpStatus.CREATED)
+    public Student createStudent(@RequestBody Student student) {
+        System.out.println(student.getId());
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
+    }
+
+    @PutMapping("/students/{id}/update")
+    public Student updateStudent(@RequestBody Student student, @PathVariable int id) {
+        System.out.println(student.getFirstName());
+        System.out.println(student.getLastName());
+        return student;
     }
 
 }
